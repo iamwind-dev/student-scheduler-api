1225896aa9ae6c90240cbe6f69fe7f438aecec6b
phanhoailang <hoailang.phan@gmail.com>
2025-12-20 01:15:56 +0700

    chore: trigger Azure Functions redeploy

--- index.js
+++ index.js
@@ -6,3 +6,4 @@ require('./src/functions/auth/index');
 require('./src/functions/api-docs/index');
 require('./src/functions/health/index');
 require('./src/functions/schedules/index');
+// Deploy trigger: Sat Dec 20 01:15:56 AM +07 2025
